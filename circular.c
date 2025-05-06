b59635307be76e2f4ffa44935d8068049c0a3948
Rishi Kumar <rishikumar10dec@gmail.com>
2025-05-06 18:52:17 +0530

    Basic structure of a driver ready

--- circular.c
+++ circular.c
@@ -1,19 +1,53 @@
 #include <linux/init.h>
 #include <linux/module.h>
+#include <linux/fs.h>
+#include <linux/kernel.h>
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Rishi");
 MODULE_DESCRIPTION("Character Device with Circular Queue");
 
+static ssize_t my_read(struct file *f, char __user *u, size_t, loff_t *o)
+{
+	pr_info("circular_chrdev: Read operation performed\n");
+	return 0;
+}
+
+static int my_open(struct inode *inode, struct file *f)
+{
+	pr_info("circular_chrdev: Open operation performed\n");
+	return 0;
+}
+
+static int my_release(struct inode *inode, struct file *f)
+{
+	pr_info("circular_chrdev: Release operation performed\n");
+	return 0;
+}
+
+static struct file_operations fops = {
+	.open = my_open,
+	.read = my_read,
+	.release = my_release
+};
+
+static int major;
+
 static int __init my_init(void)
 {
-    pr_info("Module loaded\n");
-    return 0;
+	major = register_chrdev(major, "circular_chrdev", &fops);
+	if(major < 0) {
+		pr_err("circular_chrdev: Unable to get major number\n");
+		return 0;
+	}
+	pr_info("circular_chrdev: Major number for device: %d\n", major);
+	return 0;
 }
 
 static void __exit my_exit(void)
 {
-    pr_info("Module unloaded\n");
+	unregister_chrdev(major, "circular_chrdev");
+	pr_info("Goodbye Cruel World\n");
 }
 
 module_init(my_init);
